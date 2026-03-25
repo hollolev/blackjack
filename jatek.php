a3cc0b447a21ceba63765b1c1d3e3771ddf7af18
hollolev <167965540+hollolev@users.noreply.github.com>
2026-03-25 11:12:39 +0100

    260325

--- jatek.php
+++ jatek.php
@@ -18,11 +18,11 @@
     <div class="container">
         <h2>Osztó</h2>
         <div class="dealer-cards">
-            <div class="card-dealer" style="background-color: darkgray"; id="cardplayer1id" onclick="myFunction()">
+            <div class="card-dealer" id="cardplayer1id">
                 <p class="number" id="kartya1"></p>
             </div>
 
-            <div class="card-dealer" style="background-color: darkgray;" id="cardplayer2id" onclick="myFunction2()">
+            <div class="card-dealer" style="background-color: darkgray;" id="cardplayer2id">
                 <p class="number" id="kartya2"></p>
             </div>
         </div>
@@ -38,53 +38,113 @@
         </div>
 
         <h2>Játékos</h2>
-        <div class="player-cards">
+        <div id="player_cards" class="player-cards">
 
             <div class="card-player">
-                <p class="number"> <?php echo rand(2, 11); ?> </p>
+                <p class="playernumber" id="p_num1"></p>
             </div>
 
             <div class="card-player">
-                <p class="number"> <?php echo rand(2, 11); ?> </p>
+                <p class="playernumber" id="p_num2"></p>
             </div>
         </div>
 
-        <button class="hit">Ütés</button>
+        <button class="hit" onclick="hit()">Ütés</button>
 
-        <form method="post">
-            <button class="stand">Tart</button>
-        </form>
+        <button class="stand" onclick="stand()">Tart</button>
+
+        <p id="osztolapok"></p>
+        <p id="jatekoslapok"></p>
     </div>
 
     <script>
-        let clicked1 = false;
-
-        function myFunction() {
-            if (clicked1) return;
-            clicked1 = true;
-            const randomSzam1 = Math.floor(Math.random() * 10) + 2;
-            document.getElementById("cardplayer1id").style.backgroundColor = "white";
-            const elem = document.getElementById("kartya1");
-            elem.innerHTML = randomSzam1;
+        function randomSzam() {
+            const min = 2;
+            const max = 11;
+            return Math.floor(Math.random() * (max - min + 1)) + min;
         }
 
-        let clicked2 = false;
+        function csekk(number) {
+            const lista = document.getElementsByClassName(number);
+            var x = 0;
+            for (let index = 0; index < lista.length; index++) {
+                const element = lista[index];
+                x = x + element.textContent * 1;
+            }
+            console.log(x);
+            return x;
+        }
 
-        function myFunction2() {
-            if (clicked2) return;
-            clicked2 = true;
+        function vizsgalat() {
+            if (csekk("number") > csekk("playernumber") && csekk("playernumber") > !21 && csekk("number") > !21) {
+                alert("Az Osztó nyert!");
+            } else if (csekk("playernumber") > csekk("number") && csekk("playernumber") > !21 && csekk("number") > !21) {
+                alert("A Játékos nyert!");
+            } else {
+                alert("Döntetlen!");
+            }
+        }
+
+        let clicked = false;
+
+        function stand() {
+            if (clicked) return;
+            clicked = true;
             const randomSzam2 = Math.floor(Math.random() * 10) + 2;
             document.getElementById("cardplayer2id").style.backgroundColor = "white";
             const elem = document.getElementById("kartya2");
             elem.innerHTML = randomSzam2;
+
+            csekk("number");
+
+            setTimeout(() => {
+                vizsgalat();
+            }, 1000);
         }
 
-        fetch("new.php")
-        .then( res => res.text() )
-        .then( dat => { main(dat); })
-        function main(x){
-            console.log(x);
+        function hit() {
+            if (clicked) return;
+            const div = document.createElement("div");
+            const p = document.createElement("p");
+            div.classList.add("card-player");
+            p.classList.add("playernumber");
+            const random = randomSzam();
+            p.textContent = random;
+
+            document.getElementById("player_cards").appendChild(div);
+            div.appendChild(p);
+
+            const osszeg = csekk("playernumber");
+
+            if (osszeg >= 21) {
+                stand();
+            }
         }
+
+        const elem1 = document.getElementById("p_num1");
+        elem1.innerHTML = randomSzam();
+        const elem2 = document.getElementById("p_num2");
+        elem2.innerHTML = randomSzam();
+        const elem3 = document.getElementById("kartya1");
+        elem3.innerHTML = randomSzam();
+
+        window.onload = () => {
+            csekk("number");
+            csekk("playernumber");
+        }
+
+
+
+
+        // fetch("new.php")
+        //     .then(res => res.text())
+        //     .then(dat => {
+        //         main(dat);
+        //     })
+
+        // function main(x) {
+        //     console.log(x);
+        // }
     </script>
 
 </body>
